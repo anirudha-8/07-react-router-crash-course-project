1c36a303254651922b9be8ed1d00f2b934db4f37
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-05 14:42:53 +0530

    complete route setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layout.jsx";
 import Home from "./components/Home/Home.jsx";
@@ -31,8 +30,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<RouterProvider router={router}>
-			<App />
-		</RouterProvider>
+		<RouterProvider router={router} />
 	</StrictMode>
 );
