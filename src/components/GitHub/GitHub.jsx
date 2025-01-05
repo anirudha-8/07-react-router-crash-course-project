9908026ced759a35be0acdde942542d1cf578196
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-05 21:23:56 +0530

    get github follwers and avatar image

--- src/components/GitHub/GitHub.jsx
+++ src/components/GitHub/GitHub.jsx
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
 
 const GitHub = () => {
-	const [data, setData] = useState("");
+	const [data, setData] = useState({});
 
 	useEffect(() => {
 		fetch("https://api.github.com/users/anirudha-8")
-			.then((res) => res.json)
-			.then((userData) => setData(userData));
+			.then((res) => res.json())
+			.then((userData) => {
+				console.log(userData);
+				setData(userData);
+			});
 	}, []);
 
 	return (
-		<div className="bg-gray-700 text-white text-center text-3xl">
-			GitHub Followers : {data.name}
-		</div>
+		<>
+			<div className="bg-gray-700 text-white text-center text-3xl">
+				GitHub Followers : {data.followers}
+			</div>
+			<div>
+				<img
+					src={data.avatar_url}
+					alt="github avatar"
+					className="w-96 rounded-full mx-auto my-4"
+				/>
+			</div>
+		</>
 	);
 };
 export default GitHub;
