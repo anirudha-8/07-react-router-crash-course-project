259b47d958f7a96c258784299061d49186cfd7a9
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-05 21:02:25 +0530

    add styling to User component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -2,6 +2,10 @@ import { useParams } from "react-router-dom";
 
 const User = () => {
 	const { userId } = useParams();
-	return <div>User: {userId}</div>;
+	return (
+		<div className="bg-gray-700 text-white text-3xl p-4">
+			User: {userId}
+		</div>
+	);
 };
 export default User;
