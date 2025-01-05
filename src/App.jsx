c086bc8d3e3d1ad4d41f4704637f6592944f16b6
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-05 14:43:15 +0530

    remove code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-	return (
-		<>
-			<h1 className="bg-green-600 p-4 text-2xl">
-				React Router Crash Course
-			</h1>
-		</>
-	);
+	return;
 }
 
 export default App;
