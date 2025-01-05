8603ed9a5180b3e687ed8cf02431383bb7dc1c52
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-05 13:26:30 +0530

    update Home component and setup routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import About from "./components/About/About.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "",
+				element: <Home />,
+			},
+			{
+				path: "contact",
+				element: <Contact />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+		],
+	},
+]);
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router}>
+			<App />
+		</RouterProvider>
+	</StrictMode>
+);
